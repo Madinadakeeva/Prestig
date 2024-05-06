848a0e4f7d8256b078040c5e242eb8de0c254ecb
Madina <madinadakeeva372@gmail.com>
2024-05-06 22:39:50 +0600

    fix: deploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Company from "./components/Company";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
-import Oplata from "./components/Oplata";
+//import Oplata from "./components/Oplata";
 import { useState } from "react";
 import Dostavka from "./components/Dostavka";
 import Cart from "./components/cart/Cart";
@@ -32,7 +32,7 @@ function App() {
                 element={<ProductsPage value={value} setValue={setValue} />}
               />
               <Route path={"/company"} element={<Company />} />
-              <Route path={"/oplata"} element={<Oplata />} />
+              {/* <Route path={"/oplata"} element={<Oplata />} /> */}
               <Route path={"/dostavka"} element={<Dostavka />} />
               <Route path={"/cart"} element={<Cart />} />
             </Route>
